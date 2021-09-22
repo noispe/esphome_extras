e4f50b6f0ebac328abed3d7867549ed806a19b4e
Ian Geiser <geiseri@geekcentral.pub>
2021-09-22 21:55:46 +0000

    Calibration madness

--- components/mq9_circut/mq9_circut.cpp
+++ components/mq9_circut/mq9_circut.cpp
@@ -13,7 +13,8 @@ void Mq9Circut::update_co() {
   float volts = this->sample();
   ESP_LOGD(TAG, "'%s': Got voltage=%.2fV", this->co_sensor_->get_name().c_str(),
            volts);
-  this->co_sensor_->publish_state(this->calculate_ppm(calibration::gas_type::CO, volts));
+  this->co_sensor_->publish_state(
+      this->calculate_ppm(calibration::gas_type::CO, volts));
 }
 
 void Mq9Circut::update_tvoc() {
@@ -23,60 +24,49 @@ void Mq9Circut::update_tvoc() {
   float volts = this->sample();
   ESP_LOGD(TAG, "'%s': Got voltage=%.2fV",
            this->tvoc_sensor_->get_name().c_str(), volts);
-  this->tvoc_sensor_->publish_state(this->calculate_ppm(calibration::gas_type::LPG, volts) * 1000);
+  this->tvoc_sensor_->publish_state(
+      this->calculate_ppm(calibration::gas_type::LPG, volts) * 1000);
 }
 
-float Mq9Circut::calculate_ppm(calibration::gas_type type, float voltage) {
-  float range = this->burnoff_ ? 5.0f : 1.5f;
-  float rs_gas =
-      ((range * 10.0f) / voltage) - 10.0f; // Get value of RS in a gas
-  float ratio = rs_gas / this->r0_;        // Get ratio rs_gas/rs_air
+float Mq9Circut::calculate_ppm(calibration::gas_type type, float VRl) {
+  float rs_gas = ((Vc - VRl) / VRl) * Rl; // Get value of RS in a gas
+  float ratio = rs_gas / this->r0_;       // Get ratio rs_gas/rs_air
+  float ppm = powf(10, (log10f(ratio) - calibration::setpoints[type].b) /
+                           calibration::setpoints[type].m);
 
-  float ppm = powf(10, (log10f(ratio) - calibration::setpoints[type].b) / calibration::setpoints[type].m);
-
-  ESP_LOGCONFIG(TAG, "calculate_ppm %d '%.2fV' scale: '%.2f'.", (int)type,
-                voltage, ppm);
+  ESP_LOGCONFIG(TAG, "calculate_ppm %d '%.2fV' scale: '%.2f'.", (int)type, VRl,
+                ppm);
   return ppm;
 }
 
 void Mq9Circut::setup() {
   ESP_LOGCONFIG(TAG, "Setting up MQ9 circut '%s'...", this->get_name().c_str());
+#ifdef ARDUINO_ARCH_ESP32
+  analogSetPinAttenuation(this->adc_pin_, ADC_11db);
+#endif
   this->control_pin_->setup();
   this->control_pin_->digital_write(false);
-  if (this->r0_ == 0) {
+  if (this->calibration_sensor_ != nullptr) {
     this->start_calibration();
-  }
-
-  set_interval(300, [this]() {
-    if (this->calibrating_) {
+    set_interval(30000, [this]() {
+      this->calibration_sensor_->publish_state(this->sample_calibration());
+    });
+  } else {
+    set_interval(30000, [this]() {
       this->count_++;
-      if (this->count_ < 10) { // warm up 30sec
-        return;
-      }
-      this->calibration_accumulator_ += this->sample();
-      if (this->count_ > 810) { // 2min
-        this->finish_calibration();
+      if (this->burnoff_ && this->count_ > 2) {
+        ESP_LOGD(TAG, "Sample tvoc %d, %d", this->burnoff_, this->count_);
+        this->toggle_burnoff();
+        this->update_tvoc();
         this->count_ = 0;
+      } else if (!this->burnoff_ && this->count_ > 3) {
+        ESP_LOGD(TAG, "Sample co %d, %d", this->burnoff_, this->count_);
+        this->update_co();
+        this->count_ = 0;
+        this->toggle_burnoff();
       }
-      return;
-    }
-    if (this->r0_ == 0) {
-      this->start_calibration();
-      return;
-    }
-    this->count_++;
-    if (this->burnoff_ && this->count_ > 200) {
-      ESP_LOGD(TAG, "Sample tvoc %d, %d", this->burnoff_, this->count_);
-      this->toggle_burnoff();
-      this->update_tvoc();
-      this->count_ = 0;
-    } else if (!this->burnoff_ && this->count_ > 300) {
-      ESP_LOGD(TAG, "Sample co %d, %d", this->burnoff_, this->count_);
-      this->update_co();
-      this->count_ = 0;
-      this->toggle_burnoff();
-    }
-  });
+    });
+  }
 }
 
 void Mq9Circut::dump_config() {
@@ -90,24 +80,13 @@ void Mq9Circut::dump_config() {
 
 void Mq9Circut::toggle_burnoff() {
   this->burnoff_ = !this->burnoff_;
-#ifdef ARDUINO_ARCH_ESP32
-  if (this->burnoff_) {
-    analogSetPinAttenuation(this->adc_pin_, ADC_11db);
-  } else {
-    analogSetPinAttenuation(this->adc_pin_, ADC_2_5db);
-  }
-#endif
   this->control_pin_->digital_write(!this->burnoff_);
 }
 
 float Mq9Circut::sample() {
 #ifdef ARDUINO_ARCH_ESP32
   float value_v = analogRead(this->adc_pin_) / 4095.0f; // NOLINT
-  if (this->burnoff_) {
-    value_v *= 3.9f;
-  } else {
-    value_v *= 1.1f;
-  }
+  value_v *= 3.9f;
   return value_v;
 #endif
 
@@ -119,29 +98,24 @@ float Mq9Circut::sample() {
 float Mq9Circut::get_setup_priority() const { return setup_priority::DATA; }
 
 void Mq9Circut::start_calibration() {
-  ESP_LOGD(TAG, "Start R0 calibration");
-  this->calibrating_ = true;
-  this->calibration_accumulator_ = 0.0f; // Define variable for analog readings
-  this->burnoff_ = false;
-  this->toggle_burnoff();
+  ESP_LOGD(TAG, "Start R0 calibration sample");
+  this->burnoff_ = true;
+  this->control_pin_->digital_write(!this->burnoff_);
+  this->calibration_accumulator_.reset();
   delayMicroseconds(500);
 }
 
-void Mq9Circut::finish_calibration() {
-  ESP_LOGD(TAG, "Finish R0 calibration");
-  float sensor_volt =
-      this->calibration_accumulator_ / 800.0f; // Take average of readings
+float Mq9Circut::sample_calibration() {
+  ESP_LOGD(TAG, "Finish R0 calibration sample");
+  this->calibration_accumulator_.accumulate(this->sample());
+  float r0 = 0.0f;
+  float sensor_volt = this->calibration_accumulator_.current();
   if (sensor_volt != 0.0f) {
-    float rs_air =
-        ((5.0f * 10.0f) / sensor_volt) - 10.0f; // Calculate RS in fresh air
-    this->r0_ = rs_air / 4.4f;                  // Calculate R0
-    ESP_LOGD(TAG, "Calibrated R0=%.2f", this->r0_);
-  } else {
-    ESP_LOGD(TAG, "Failed sensor voltage=%.2f (%.2f)", sensor_volt,
-             this->calibration_accumulator_);
+    float rs_air = ((Vc * Rl) / sensor_volt) - Rl; // Calculate RS in fresh air
+    r0 = rs_air / 10.0f;                           // Calculate R0 for air
+    ESP_LOGD(TAG, "Calibrated R0=%.2f voltage=%.2f", r0, sensor_volt);
   }
-  this->toggle_burnoff();
-  this->calibrating_ = false;
+  return r0;
 }
 
 #ifdef ARDUINO_ARCH_ESP8266
--- components/mq9_circut/sensor.py
+++ components/mq9_circut/sensor.py
@@ -9,6 +9,7 @@ from esphome.const import (
     CONF_TVOC,
     DEVICE_CLASS_EMPTY,
     DEVICE_CLASS_CARBON_MONOXIDE,
+    DEVICE_CLASS_GAS,
     ICON_CHEMICAL_WEAPON,
     STATE_CLASS_MEASUREMENT
 )
@@ -19,26 +20,31 @@ CONF_ADC_PIN = "adc_pin"
 CONF_CONTROL_PIN = "control_pin"
 CONF_CO = "carbon_monoxide"
 CONF_R0 = "r0"
-
+CONF_R0_CALIBRATE = "r0_calibrate"
 mq9_circut_ns = cg.esphome_ns.namespace("mq9_circut")
 Mq9Circut = mq9_circut_ns.class_(
     "Mq9Circut", sensor.Sensor, cg.Component, voltage_sampler.VoltageSampler
 )
 
-CONFIG_SCHEMA = (
+CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(Mq9Circut),
             cv.Required(CONF_ADC_PIN): pins.analog_pin,
             cv.Required(CONF_CONTROL_PIN): pins.gpio_output_pin_schema,
-            cv.Optional(CONF_R0, default=0): cv.float_range(
-                min=0, max=100
+            cv.Optional(CONF_R0): cv.float_range(
+                min=0.001, max=1000
+            ),
+            cv.Optional(CONF_R0_CALIBRATE): sensor.sensor_schema(
+                accuracy_decimals=3,
+                device_class=DEVICE_CLASS_EMPTY,
+                state_class=STATE_CLASS_MEASUREMENT,
             ),
             cv.Optional(CONF_TVOC): sensor.sensor_schema(
                 UNIT_PARTS_PER_BILLION,
                 ICON_CHEMICAL_WEAPON,
                 2,
-                DEVICE_CLASS_EMPTY,
+                DEVICE_CLASS_GAS,
                 STATE_CLASS_MEASUREMENT,
             ),
             cv.Optional(CONF_CO): sensor.sensor_schema(
@@ -48,7 +54,8 @@ CONFIG_SCHEMA = (
                 DEVICE_CLASS_CARBON_MONOXIDE,
                 STATE_CLASS_MEASUREMENT,
             )
-        }).extend(cv.COMPONENT_SCHEMA)
+        }).extend(cv.COMPONENT_SCHEMA),
+    cv.has_exactly_one_key(CONF_R0_CALIBRATE, CONF_R0),
 )
 
 
@@ -60,6 +67,9 @@ async def to_code(config):
     cg.add(var.set_control_pin(control_pin))
     if CONF_R0 in config:
         cg.add(var.set_r0(config[CONF_R0]))
+    if CONF_R0_CALIBRATE in config:
+        sens = await sensor.new_sensor(config[CONF_R0_CALIBRATE])
+        cg.add(var.set_calibration_sensor(sens))
     if CONF_TVOC in config:
         sens = await sensor.new_sensor(config[CONF_TVOC])
         cg.add(var.set_tvoc_sensor(sens))
