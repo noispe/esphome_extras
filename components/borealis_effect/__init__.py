5d34b872e2935b3e3ef5c0e2abfa11a6112a5014
Ian Geiser <geiseri@geekcentral.pub>
2022-01-15 01:35:25 +0000

    blah

--- components/borealis_effect/__init__.py
+++ components/borealis_effect/__init__.py
@@ -14,18 +14,21 @@ CONFIG_SCHEMA = cv.All(cv.Schema({}))
     BorealisLightEffect,
     "BOREALIS",
     {
-        cv.Optional(CONF_WIDTH, default=1): cv.positive_int(1),
-        cv.Optional(CONF_WEIGHT, default=1): cv.positive_int(1),
-        cv.Optional(CONF_NUM_LEDS, default=1): cv.positive_int(1),
-        cv.Optional(CONF_SPEED, default=3): cv.positive_int(1),
+        CONF_NUM_LEDS: cv.positive_int(1),
+        cv.Optional(CONF_WIDTH): cv.positive_int(1),
+        cv.Optional(CONF_WEIGHT): cv.positive_int(1),
+        cv.Optional(CONF_SPEED): cv.positive_int(1),
     },
 )
 
 async def borealis_light_effect_to_code(config, effect_id):
     effect = cg.new_Pvariable(effect_id, config[CONF_NAME])
-    effect.set_width_factor(config[CONF_WIDTH])
-    effect.set_color_weight_preset(config[CONF_WEIGHT])
-    effect.set_speed_factor(config[CONF_SPEED])
+    if config[CONF_WIDTH]:
+        effect.set_width_factor(config[CONF_WIDTH])
+    if config[CONF_WEIGHT]:
+        effect.set_color_weight_preset(config[CONF_WEIGHT])
+    if config[CONF_SPEED]:
+        effect.set_speed_factor(config[CONF_SPEED])
     effect.set_num_leds(config[CONF_NUM_LEDS])
     cg.add(effect)
     return effect
