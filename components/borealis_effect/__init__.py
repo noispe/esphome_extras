cc8587b1df1909a12a7f19b00d6f9f915d060319
Ian Geiser <geiseri@geekcentral.pub>
2022-01-15 02:11:53 +0000

    blah

--- components/borealis_effect/__init__.py
+++ components/borealis_effect/__init__.py
@@ -15,7 +15,7 @@ CONFIG_SCHEMA = cv.Schema({})
     BorealisLightEffect,
     "Borealis Effect",
     {
-        cv.Required(CONF_NUM_LEDS): cv.positive_int(1),
+        cv.Required(CONF_NUM_LEDS): cv.int_range(min=25, max=65535),
         cv.Optional(CONF_WIDTH, default=1): cv.int_range(1, 50),
         cv.Optional(CONF_WEIGHT, default=1): cv.int_range(1, 25),
         cv.Optional(CONF_SPEED, default=3): cv.int_range(1, 25),
