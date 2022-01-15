7e6997975736bde5ba0cc23239baee8c559b7671
Ian Geiser <geiseri@geekcentral.pub>
2022-01-15 01:18:46 +0000

    adding new effect

--- components/borealis_effect/__init__.py
+++ components/borealis_effect/__init__.py
@@ -1,25 +1,31 @@
-from esphome.jsonschema import jschema_extractor
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import automation
-from esphome.const import CONF_ID
+from esphome.components.light.types import AddressableLightEffect
+from esphome.components.light.effects import register_addressable_effect
+from esphome.const import CONF_NAME, CONF_WIDTH, CONF_SPEED, CONF_WEIGHT, CONF_NUM_LEDS
 
-ledeffect_ns = cg.esphome_ns.namespace("ledeffect")
-LedEffect = ledeffect_ns.class_(
-    "LedEffect", cg.PollingComponent
+borealis_ns = cg.esphome_ns.namespace("borealis")
+BorealisLightEffect = borealis_ns.class_("BorealisLightEffect", AddressableLightEffect)
+
+CONFIG_SCHEMA = cv.All(cv.Schema({}))
+
+@register_addressable_effect(
+    "borealis",
+    BorealisLightEffect,
+    "BOREALIS",
+    {
+        cv.Optional(CONF_WIDTH, default=1): cv.positive_int(1),
+        cv.Optional(CONF_WIDTH, default=1): cv.positive_int(1),
+        cv.Optional(CONF_NUM_LEDS, default=1): cv.positive_int(1),
+        cv.Optional(CONF_SPEED, default=3): cv.positive_int(1),
+    },
 )
 
-LedEffect = cg.esphome_ns.struct("Color")
-
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.Required(CONF_ID): cv.declare_id(ColorStruct),
-
-    }
-).extend(cv.COMPONENT_SCHEMA)
-
-
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-
+async def borealis_light_effect_to_code(config, effect_id):
+    effect = cg.new_Pvariable(effect_id, config[CONF_NAME])
+    effect.set_width_factor(config[CONF_WIDTH])
+    effect.set_color_weight_preset(config[CONF_WEIGHT])
+    effect.set_speed_factor(config[CONF_SPEED])
+    effect.set_num_leds(config[CONF_NUM_LEDS])
+    cg.add(effect)
+    return effect
--- components/borealis_effect/borealis_wave.h
+++ components/borealis_effect/borealis_wave.h
@@ -1,6 +1,8 @@
 #pragma once
+
 #include "esphome/core/component.h"
 #include "esphome/core/color.h"
+#include "esphome/components/light/addressable_light_effect.h"
 
 namespace esphome {
 
@@ -51,8 +53,31 @@ class BorealisWave {
   uint8_t wave_speed_factor_ = 1;         // Higher number, higher speed
 };
 
+namespace borealis {
 
-namespace LedEffect {
+class BorealisLightEffect : public light::AddressableLightEffect {
+ public:
+  BorealisLightEffect(const std::string &name);
 
-}
+ public:
+  void start() override;
+  void stop() override;
+  void apply(light::AddressableLight &it, const Color &current_color) override;
+
+  void set_width_factor(const uint8_t &width_factor) { width_factor_ = width_factor; }
+  void set_color_weight_preset(const uint8_t &color_weight_preset) { color_weight_preset_ = color_weight_preset; }
+  void set_speed_factor(const uint8_t &speed_factor) { speed_factor_ = speed_factor; }
+  void set_num_leds(const uint8_t &num_leds) { num_leds_ = num_leds; }
+
+ private:
+  void blank_all_leds_(light::AddressableLight &it);
+
+  uint8_t num_leds_ = 1;  // Number of LEDS on strip
+  uint8_t width_factor_;
+  uint8_t color_weight_preset_;
+  uint8_t speed_factor_;
+  std::shared_ptr<BorealisWave> wave_ = nullptr;
+};
+
+}  // namespace borealis
 }  // namespace esphome
